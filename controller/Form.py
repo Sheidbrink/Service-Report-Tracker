9d8fb892ec3cfc18b85d53aa7262e14d51d1b87d
Scott <scott.heidbrink@gmail.com>
2014-03-24 22:15:52 -0600

    Added Database Communicator and a few administrative files.

--- controller/Form.py
+++ controller/Form.py
@@ -25,9 +25,11 @@ class Form(object):
         self._info["Name"] = name
         for item in kwargs:
             self._info[item] = kwargs[item]
+
     '''Add category or form info'''
     def AddCategory(self, key, value=None):
         self._info[key] = value
+
     '''Add item to Category'''
     def AddItem(self, category, key, value=None):
         if category not in self._info:
@@ -38,13 +40,16 @@ class Form(object):
             self._info[category][key] = value
             return True
         return False
+
     def GetValue(self, key, keyTwo=None):
         if key in self._info:
             if keyTwo is not type(None) and keyTwo in self._info[key]:
                 return self._info[key][keyTwo]
             return self._info[key]
         return None
+
     def GetRootKeys(self):
         return self._info.keys()
+
     def GetInfo(self):
         return self._info
---
+++ models/DBCommunicator.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+from controller.Form import Form
+
+''' Each collection is a collection of Forms
+    Perhaps each database is user or company '''
+
+class DBCommunicator(object):
+    # select where to connect to the database and the database to connect to
+    # Perhaps have each user who logs in as a different database?
+    def __init__(self, dbname, hostname='localhost', port=27017):
+        self.client = MongoClient(hostname, port)
+        self.db = client[dbname]
+
+    def AddForm(self, form)
+        if type(form) is Form.Form:
+            collection = self.db[form['Name'].replace(' ','-')]
+            collection.insert(form)
+            return True
+        else:
+            return False
+
+    def Find(self, search, formName)
+        formName = formName.replace(' ','-')
+        if type(search) is type({}):
+            collection = self.db[formName]
+            forms = []
+            for form in collection.find(search):
+                forms.append(form)
+            return forms
+        return []
+
+    def GetAllForms(self, formName)
+        formName = formName.replace(' ','-')
+        collection = self.db[formName]
+        return collection.find()
