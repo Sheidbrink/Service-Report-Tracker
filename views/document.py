1b538e26e26c037eb9675e333dabd904e9dafc1d
Scott Heidbrink <scott.heidbrink@gmail.com>
2014-04-20 01:58:41 -0600

    Fixed submitting Fixed the submit with the new refactored version of submit and search. All that should be left for basic functionality is displaying search results correctly.

--- views/document.py
+++ views/document.py
@@ -5,14 +5,42 @@ document = Blueprint('document', __name__)
 
 
 @document.route('/create/<formName>')
-def renderDocument(formName):
+def createDocument(formName):
 	allFields  = loadDocument(formName)
-	return render_template('documentPage.html', active='bdocs', \
-		formName=formName, allFields=allFields, formValues=docValues, selected=selected)
+	return render_template('documentPage.html', formName=formName, \
+		allFields=allFields, active='create', buttonName="Submit", \
+		submitURL=url_for('document.submitdoc'))
+	#return render_template('documentPage.html', active='bdocs', \
+		#formName=formName, allFields=allFields, formValues=docValues, selected=selected)
 
-@document.route('/search', methods=['POST'])
-def searchdocs():
-	if(request.method == 'POST'):
+@document.route('/submit', methods=['POST'])
+def submitdoc():
+	form = {}
+	formName = request.form['formName']
+	for key in request.form.keys():
+		form[key] = request.form[key]
+	del form['formName']
+	del form['Submit']
+	form['submittedOn'] = datetime.datetime.utcnow()
+	successful = submit(formName, form)
+	if successful:
+		flash('Submitted Successfully')
+		return redirect(url_for('document.createDocument', \
+			formName=formName))
+	else:
+		flash('Error submitting')
+		#perhaps keep values filled in here
+		return redirect(url_for('document.createDocument', \
+			formName=formName))#, docValues=form)
+
+@document.route('/search/<formName>', methods=['POST', 'GET'])
+def searchdocs(formName):
+	if (request.method == 'GET'):
+		allFields = loadDocument(formName)
+		return render_template('documentPage.html', formName=formName, \
+			allFields=allFields, active='search', buttonName="Search", \
+			submitURL=url_for('document.searchdocs', formName=formName))
+	elif (request.method == 'POST'):
 		form = {}
 		formName = request.form['formName']
 		
@@ -20,32 +48,18 @@ def searchdocs():
 			form[key] = request.form[key]
 		del form['formName']
 		#Search
-		if "search" in request.form:
-			del form['search']
-			matchedDict = search(formName, form)
-			if(len(matchedDict) >= 1):
-				cdate = matchedDict[0]['submittedOn']
-				flash(str(cdate.date()) + ' ' + str(cdate.hour) + \
-					':' + str(cdate.minute) + ':' + str(cdate.second) + ' - Search Successful')
-				return renderDocument(formName, matchedDict, 0)
-			else:
-				flash('Nothing Matching Search')
-				return redirect(url_for('document.renderDocument', \
-					document=formName))
-		#Submit
-		elif "submit" in request.form:
-			del form['submit']
-			form['submittedOn'] = datetime.datetime.utcnow()
-			successful = submit(formName, form)
-			if successful:
-				flash('Submitted Successfully')
-				return redirect(url_for('document.renderDocument', \
-					document=formName))
-			else:
-				flash('Error submitting')
-		#Something Else
-		flash('Unexpected Error')
-		return renderDocument(document=formName, docValues=form)
+		del form['search']
+		matchedDict = search(formName, form)
+		if(len(matchedDict) >= 1):
+			cdate = matchedDict[0]['submittedOn']
+			flash(str(cdate.date()) + ' ' + str(cdate.hour) + \
+				':' + str(cdate.minute) + ':' + str(cdate.second) + ' - Search Successful')
+			return createDocument(formName, matchedDict, 0)
+		else:
+			flash('Nothing Matching Search')
+			return redirect(url_for('document.createDocument', \
+				document=formName))
+			
 
 @document.route('/api', methods=['GET', 'POST'])
 def api():
