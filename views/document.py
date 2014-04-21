f306e8a62e96bd2bb7b1fcc8e717779f12ba93f7
Scott Heidbrink <scott.heidbrink@gmail.com>
2014-04-21 02:13:55 -0600

    Changed to use cookies and turned off debugging Rather than by global variable

--- views/document.py
+++ views/document.py
@@ -1,16 +1,28 @@
-from flask import Flask, Blueprint, render_template, session, url_for, request, flash, redirect
+from flask import Flask, session, Blueprint, render_template, session, url_for, request, flash, redirect
 from controller.controller import search, submit, loadDocument
 import datetime
 import json
 document = Blueprint('document', __name__)
 
-searchedValues = []
+def getFormKeys(formName):
+	allFields = loadDocument(formName)
+	allkeys = []
+	for field in allFields:
+		if field is type(()):
+			continue
+		allkeys.append(field[1])
+	return allkeys
+
+def getSearchedValues(formName):
+	toReturn = {}
+	for field in getFormKeys(formName):
+		toReturn[field] = session.get(field)
+	return search(formName, toReturn)
 
 @document.route('/create/<formName>')
 def createDocument(formName):
-	allFields  = loadDocument(formName)
 	return render_template('documentPage.html', formName=formName, \
-		allFields=allFields, active='create', buttonName="Submit", \
+		allFields=loadDocument(formName), active='create', buttonName="Submit", \
 		submitURL=url_for('document.submitdoc'))
 	#return render_template('documentPage.html', active='bdocs', \
 		#formName=formName, allFields=allFields, formValues=docValues, selected=selected)
@@ -20,9 +32,8 @@ def submitdoc():
 	form = {}
 	formName = request.form['formName']
 	for key in request.form.keys():
-		form[key] = request.form[key]
-	del form['formName']
-	del form['Submit']
+		if 'formName' and 'Submit' not in key:
+			form[key] = request.form[key]
 	form['submittedOn'] = datetime.datetime.utcnow()
 	successful = submit(formName, form)
 	if successful:
@@ -38,30 +49,23 @@ def submitdoc():
 @document.route('/display/<formName><selectedIndex>', methods=['GET'])
 def displaydoc(formName, selectedIndex):
 	selectedIndex = int(selectedIndex)
-	allFields = loadDocument(formName)
 	return render_template('search.html', formName=formName, \
-		allFields=allFields, active='search', formValues=searchedValues, \
-		selectedIndex=selectedIndex)
+		allFields=loadDocument(formName), active='search', \
+		formValues=getSearchedValues(formName), selectedIndex=selectedIndex)
 
 @document.route('/search/<formName>', methods=['POST', 'GET'])
 def searchdocs(formName):
 	if (request.method == 'GET'):
-		allFields = loadDocument(formName)
 		return render_template('documentPage.html', formName=formName, \
-			allFields=allFields, active='search', buttonName="Search", \
+			allFields=loadDocument(formName), active='search', buttonName="Search", \
 			submitURL=url_for('document.searchdocs', formName=formName))
 	elif (request.method == 'POST'):
-		form = {}
 		formName = request.form['formName']
 		for key in request.form.keys():
-			form[key] = request.form[key]
-		del form['formName']
-		del form['Search']
-		matchedDict = search(formName, form)
-		global searchedValues
-		searchedValues = matchedDict
+			if 'formName' and 'Search' not in key:
+				session[key] = request.form[key]
+		matchedDict = getSearchedValues(formName)
 		if(len(matchedDict) >= 1):
-			allFields = loadDocument(formName)
 			return redirect(url_for('document.displaydoc', \
 				formName=formName, selectedIndex=0))
 		else:
@@ -75,11 +79,11 @@ def api():
 	if 'formName' not in request.headers:
 		return 'No \'formName\' given'
 	formName = request.headers['formName']
-	allFields = loadDocument(formName)
+	allFormKeys = getFormKeys(formName)
 	form = {}
 	request.headers.keys()
 	for key in request.headers.keys():
-		if key in allFields:
+		if key in allFormKeys:
 			form[key] = request.headers[key]
 	if request.method == 'GET':
 		matched = search(formName, form)
